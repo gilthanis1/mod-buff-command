5522becdc996a24c32e019f27dd929ccab6b7f63
gilthanis1 <jmrichardson917@gmail.com>
2024-01-25 15:33:59 -0500

    Update buffs to stop at 70 level

--- sql/world/base/structure.sql
+++ sql/world/base/structure.sql
@@ -7,12 +7,12 @@ CREATE TABLE `player_buff`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- DATA
-INSERT INTO `player_buff` VALUES (25898, NULL);
-INSERT INTO `player_buff` VALUES (48074, NULL);
-INSERT INTO `player_buff` VALUES (48162, NULL);
-INSERT INTO `player_buff` VALUES (48470, NULL);
-INSERT INTO `player_buff` VALUES (57623, NULL);
-INSERT INTO `player_buff` VALUES (61316, NULL);
+INSERT INTO `player_buff` VALUES (25898, 'Greater Kings - R3'); -- Greater Blessing of Kings (R3)
+INSERT INTO `player_buff` VALUES (32999, 'Spirit - R2'); -- Prayer of Spirit - R2 
+INSERT INTO `player_buff` VALUES (25392, 'Fortitude - R3'); -- Prayer of Fortitude (R3)
+INSERT INTO `player_buff` VALUES (26991, 'GoTWild - R3'); -- Gift of the Wild - R3
+INSERT INTO `player_buff` VALUES (27127, 'Arcane Brilliance - R2'); -- Arcane Brilliance - R2
+-- INSERT INTO `player_buff` VALUES (61316, NULL); -- Not used at the moment
 
 -- COMMAND
 INSERT INTO `command`(`name`, `security`, `help`) VALUES ('buff', 0, 'Syntax: .buff. Buff player.\r\n.buff reload - reload player_buff table.');
